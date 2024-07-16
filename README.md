d60326d7e996fbbf1c12e1bfefe0590a196eea36
Emilio Padron <emiliopm1997@gmail.com>
2024-07-15 23:05:36 -0600

    Stream generator bugfixes

--- README.md
+++ README.md
@@ -21,3 +21,16 @@ docker network rm <network_name>
 
 Stop container
 docker stop <container_name>
+
+Build individual image
+docker build -t <image_name> <dockerfile_path>
+
+Run image in container
+docker run -it --network=<network_name> --name <container_name> <image_name>
+
+docker build -t stream_generator ./stream_generator
+docker run -it --network=motor_sensor_network --name sg_container stream_generator
+telnet localhost 9093
+curl http://127.0.0.1:5001/start_generating_values
+
+docker run --rm -v $PWD/data/kafka:/var/lib/kafka/data confluentinc/cp-kafka:latest kafka-storage.sh format -t "kafka-cluster" -c /etc/kafka/kraft/server.properties
--- stream_generator/app.py
+++ stream_generator/app.py
@@ -4,18 +4,21 @@ import pandas as pd
 import threading
 import time
 
-from confluent_kafka import Producer
+from kafka import KafkaProducer
 from flask import Flask, jsonify
 
-from .helper_functions import ts_to_unix, unix_to_ts
-from .voltage_simulator import VoltageSensorSimulator
+from helper_functions import ts_to_unix, unix_to_ts
+from logger import LOGGER
+from voltage_simulator import VoltageSensorSimulator
 
 app = Flask(__name__)
 
 KAFKA_SERVER = 'kafka:9092'
 TOPIC = 'motor_voltage'
 
-producer = Producer(bootstrap_servers=[KAFKA_SERVER])
+producer = KafkaProducer(
+    bootstrap_servers=[KAFKA_SERVER], api_version=(2, 0, 2)
+)
 
 
 def send_sensor_values():
@@ -23,21 +26,25 @@ def send_sensor_values():
     start = pd.Timestamp.now()
 
     # while True:
-    for _ in range(10):
+    for x in range(10):
+        LOGGER.info("Iteration {}".format(x))
         end = start + pd.Timedelta(seconds=15)
 
         # Get data and convert it.
         data = get_voltage_data(start, end)
         message = data.to_dict("records")
+        LOGGER.info("{} rows will be sent.".format(len(data)))
 
         # Send stream.
-        producer.send(TOPIC, json.dumps(message).encode())
+        producer.send(TOPIC, json.dumps(message).encode("utf-8"))
         producer.flush()
+        LOGGER.info("Message was sent at {}".format(pd.Timestamp.now()))
 
         start = end
         now = pd.Timestamp.now()
         if now < end:
             t_diff_s = (end - now).total_seconds()
+            LOGGER.info("Waiting {} seconds...".format(t_diff_s))
             time.sleep(t_diff_s)  # Wait until the current time is reached.
 
 
@@ -62,9 +69,9 @@ def get_voltage_data(start: pd.Timestamp, end: pd.Timestamp) -> pd.DataFrame:
     data["unix_time"] = np.linspace(
         ts_to_unix(start),
         ts_to_unix(end),
-        1200
+        151
     )[:-1]
-    data["date_time"] = data["unix_time"].apply(unix_to_ts)
+    data["date_time"] = data["unix_time"].apply(unix_to_ts).astype(str)
 
     # Simulate data
     simulator = VoltageSensorSimulator()
@@ -76,6 +83,8 @@ def get_voltage_data(start: pd.Timestamp, end: pd.Timestamp) -> pd.DataFrame:
 @app.route('/start_generating_values', methods=['GET'])
 def start_generating_values():
     """Generate sensor values."""
+    LOGGER.info("-" * 15)
+    LOGGER.info("Values will start to be generated.")
     # Start generating values in a background thread
     thread = threading.Thread(target=send_sensor_values)
     thread.daemon = True
