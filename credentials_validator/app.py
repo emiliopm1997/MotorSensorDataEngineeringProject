4e06795200685d52a1e66c78be7c3a57f405cc67
Emilio Padron <emiliopm1997@gmail.com>
2024-07-27 15:24:36 +0000

    Improved credentials validator microservice with isort and black

--- credentials_validator/app.py
+++ credentials_validator/app.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, jsonify
 import json
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/validate', methods=['POST'])
+@app.route("/validate", methods=["POST"])
 def validate():
     """Validate credentials."""
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get("username")
+    password = data.get("password")
 
-    with open('config.json', 'r') as json_file:
+    with open("config.json", "r") as json_file:
         true_credentials = json.load(json_file)
 
     valid_username = true_credentials["username"]
@@ -19,13 +20,13 @@ def validate():
 
     if username == valid_username and password == valid_password:
         status_code = 200
-        response = {'message': 'Login successful', 'status_code': status_code}
+        response = {"message": "Login successful", "status_code": status_code}
     else:
         status_code = 401
-        response = {'error': 'Invalid credentials', 'status_code': status_code}
+        response = {"error": "Invalid credentials", "status_code": status_code}
 
     return jsonify(response), status_code
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
