aa5f9ed39a796ad8c0d88b69a8ea51d76780f5b0
Emilio Padron <emiliopm1997@gmail.com>
2024-07-27 15:20:40 +0000

    Improved the orchestrator microservice with isort and black

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -1,27 +1,27 @@
-from flask import Flask, jsonify
 import json
-import requests
 
+import requests
+from flask import Flask, jsonify
 from logger import LOGGER
 
 app = Flask(__name__)
 
-STREAM_GENERATOR_URL = 'http://stream_generator:5001/start_generating_values'
-DATA_HANDLER_URL = 'http://data_handler:5002/{}'
-VALIDATE_CREDENTIALS_URL = 'http://credentials_validator:5003/validate'
+STREAM_GENERATOR_URL = "http://stream_generator:5001/start_generating_values"
+DATA_HANDLER_URL = "http://data_handler:5002/{}"
+VALIDATE_CREDENTIALS_URL = "http://credentials_validator:5003/validate"
 
 
-@app.route('/start_pipeline', methods=['GET'])
+@app.route("/start_pipeline", methods=["GET"])
 def start_pipeline():
     """Start the pipeline."""
     LOGGER.info("-" * 30)
-    
+
     # Start start stream generator
     LOGGER.info("Starting stream generator...")
     requests.get(STREAM_GENERATOR_URL)
 
     # Validate credentials.
-    with open('config.json', 'r') as json_file:
+    with open("config.json", "r") as json_file:
         credentials = json.load(json_file)
     LOGGER.info("Validating credentials...")
     validation_results = requests.post(
@@ -47,11 +47,10 @@ def start_pipeline():
         message = f"Error: {error_msg}"
 
     return (
-        jsonify(
-            {"message": message, "status_code": status_code}
-        ), status_code
+        jsonify({"message": message, "status_code": status_code}),
+        status_code,
     )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- orchestrator/retrieving_example.py
+++ orchestrator/retrieving_example.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import requests
 
-DATA_HANDLER_URL = 'http://data_handler:5002/{}'
+DATA_HANDLER_URL = "http://data_handler:5002/{}"
+
 
 def main():
+    """Simulate data retreival for dashboard."""
     # Get data for report. This should be done by the front end.
     print("Setting periods ...")
     info_json = dict()
@@ -11,13 +13,12 @@ def main():
     ts_end = pd.Timestamp.now() - pd.Timedelta(seconds=15)
     info_json["date_time_start"] = str(ts_start)
     info_json["date_time_end"] = str(ts_end)
-    
+
     print("Sending request ...")
     result = requests.post(
-        DATA_HANDLER_URL.format("retrieve_data_for_report"),
-        json=info_json
+        DATA_HANDLER_URL.format("retrieve_data_for_report"), json=info_json
     )
-    
+
     # Catch errors in request.
     if result.status_code in [400, 404]:
         error_msg = result.json()["error"]
@@ -25,18 +26,21 @@ def main():
             error_msg += ". Try again soon."
         print(f"Error ({result.status_code}): {error_msg}")
         return
-    
+
     # This is the way to retrieve data for a dashboard.
     print("Data retrieved successfully ...")
     raw_data = pd.DataFrame(result.json()["data"]["raw_data"])
     metrics_data = pd.DataFrame(result.json()["data"]["metrics_data"])
-    
+
     # This is only to ensure that data is being retreived.
     print(f"\nRaw data contains {len(raw_data)} lines. Showing bottom:")
     print(raw_data.tail(15))
-    print(f"\nMetrics data contains {len(metrics_data)} lines. Showing bottom:")
+    print(
+        f"\nMetrics data contains {len(metrics_data)} lines. "
+        "Showing bottom:"
+    )
     print(metrics_data.tail(15))
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
